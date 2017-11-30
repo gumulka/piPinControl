067c92a35de828b7b9560d6d0034afdff12672cd
Fabian Pflug <pflug@chi.uni-hannover.de>
2017-11-30 13:08:38 +0100

    Renaming
    Removed the old main and added the new one.
    Changed the makefile to reflect the changes

--- main.c
+++ main.c
@@ -1,117 +1,157 @@
 #include <stdio.h>
-#include <wiringPi.h>
-#include <wiringPiSPI.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include "MQTTClient.h"
+
+#define ADDRESS     "tcp://192.168.0.4:1883"
+#define CLIENTID    "ExampleClientPub"
+#define TOPIC       "/openhab/Flur_Switch_v/state"
+#define PAYLOAD1     "OPEN"
+#define PAYLOAD2     "CLOSED"
+#define QOS         2
+#define TIMEOUT     10000L
+
+MQTTClient_message open, close;
+MQTTClient client;
+
+int initPinforObserve(int pin);
+void alertFunction(int gpio, int level, uint32_t tick);
+
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) {
+    keepRunning = 0;
+}
+
+int initPinforObserve(int pin) {
+    /* set pin mode */
+    int rc = gpioSetMode(pin,PI_INPUT);
+    /* return error code if there was any */
+    if(rc!=0) {
+        return rc;
+    }
+
+    /* read and publish the current state */
+    int level = gpioRead(pin);
+    alertFunction(pin,level,0);
+
+    /* activate pin observation */
+    gpioSetAlertFunc(pin,alertFunction);
+}
+
+void alertFunction(int gpio, int level, uint32_t tick) {
+    int rc;
+    MQTTClient_deliveryToken token;
+    char topic[100];
+
+    /* create the topic from the pin number */
+    sprintf(topic,"/openhab/iopins/%d/state",gpio);
+
+    /* select the message based on pin state
+     * translation:
+     *  1->"OPEN"
+     *  0->"CLOSED"
+     */
+    MQTTClient_message m;
+    if(level==0) {
+        m = open;
+    } else if(level==1){
+        m = close;
+    } else {
+        /* ignore all other states
+         * Could be 2 if timeout hits */
+        return ;
+    }
+
+    /* publish the message and wait for completion */
+    MQTTClient_publishMessage(client, topic, &m, &token);
+    printf("Waiting for up to %d seconds for publication of %s\n"
+            "on topic %s for client with ClientID: %s\n",
+            (int)(TIMEOUT/1000), m.payload, topic, CLIENTID);
+    /// this might be not necessary. Should be evaluated!
+    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+
+    /* report to the user about errors */
+    if(rc!=0) {
+        printf("Error while sending message!\n");
+        /* don't stop the program. Just report.
+        keepRunning = 0; */
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    /* Read config */
+/*
+ * while(there is input) {
+  get one line;
+  if (it is empty line || it beings with spaces followed by a '#') {
+    skip this line, either empty or it's a comment;
+  }
+  find the position of the token that splits option name and its value;
+  copy the option name and its value to separate variables;
+  removing spaces before and after these variables if necessary;
+  if (option == option1) {
+     parse value for option 1;
+  } else if (option == option2) {
+     parse value for option 2;
+  } else {
+     handle unknown option name;
+  }
+  check consistency of options if necessary;
+} */
 
 
-#define MAX(a,b) ((a) > (b) ? a : b)
-#define MIN(a,b) ((a) < (b) ? a : b)
-
-#define PIN_OUTPUT 5
-#define BUFFER_SIZE 16
-
-#define STATE_FILE "/home/pi/light_state"
-#define LOG_FILE "/home/pi/light_log"
-
-const char *bit_rep[16] = {
-    [ 0] = "0000", [ 1] = "0001", [ 2] = "0010", [ 3] = "0011",
-    [ 4] = "0100", [ 5] = "0101", [ 6] = "0110", [ 7] = "0111",
-    [ 8] = "1000", [ 9] = "1001", [10] = "1010", [11] = "1011",
-    [12] = "1100", [13] = "1101", [14] = "1110", [15] = "1111",
-};
-
-int main(int argc, char** argv) {
-	wiringPiSetupSys();
-	if(wiringPiSPISetup (1,500000)==-1) {
-		printf("ERROR on initialising SPI");
-		return EXIT_FAILURE;
-	}
-
-	size_t bytes_read = 0;
-	unsigned char buffer[BUFFER_SIZE];
-	unsigned char rbuffer[BUFFER_SIZE];
-	FILE *file_ptr;
+    /* pigpio init */
+    if (gpioInitialise() < 0) {
+        // pigpio initialisation failed.
+        exit(EXIT_FAILURE);
+    }
 
 
-	file_ptr = fopen(STATE_FILE,"rb");
-	if (!file_ptr) {
-		printf("Unable to open file!\n");
-		return EXIT_FAILURE;
-	}
-	/* Read in 256 8-bit numbers into the buffer */
-	bytes_read = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, file_ptr);
-	fclose(file_ptr);
+    /* MQTT init */
+    MQTTClient_create(&client, ADDRESS, CLIENTID,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
 
-	int before = buffer[0];
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect, return code %d\n", rc);
+        gpioTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-	int i = 0;
-	if(argc==3) {
-		int a = 0;
-		int on = 0;
-		if(!strcmp("ON",argv[2])) {
-			a = atoi(argv[1]);
-			on = 1;
-		} else
-		if(!strcmp("OFF",argv[2])) {
-			a = atoi(argv[1]);
-		} else {
-			printf("Usage: %s PIN [ON|OFF]\n",argv[0]);
-			return EXIT_FAILURE;
-		}
+    /* Initialize the two possible messages */
+    open = MQTTClient_message_initializer;
+    open.payload = PAYLOAD1;
+    open.payloadlen = strlen(PAYLOAD1);
+    open.qos = QOS;
+    open.retained = 0;
 
-		while(a>=8) {
-			a -= 8;
-			i++;
-		}
-		if(on) {
-			buffer[i] |= (1<<a);
-		} else {
-			buffer[i] &= ~(1<<a);
-		}
+    close = MQTTClient_message_initializer;
+    close.payload = PAYLOAD2;
+    close.payloadlen = strlen(PAYLOAD2);
+    close.qos = QOS;
+    close.retained = 0;
 
-		file_ptr = fopen(STATE_FILE,"wb");
-		if (!file_ptr) {
-			printf("Unable to open file for write!\n");
-			return EXIT_FAILURE;
-		}
-		fwrite(buffer,1,bytes_read,file_ptr);
-		fclose(file_ptr);
 
-	}
-	bytes_read = MAX(i+1,bytes_read);
-	for(i=0;i<bytes_read;i++) {
-		rbuffer[i] = buffer[bytes_read-i-1];
-		printf("byte value %2d: %s%s\n",i,bit_rep[buffer[0]>>4], bit_rep[buffer[0] & 0x0F]);
-	}
+    /* set pins for observation */
+    initPinforObserve(23);
 
-	/* Write the data */
-	wiringPiSPIDataRW(1, rbuffer,bytes_read);
+    /* init signal handling */
+    signal(SIGINT, intHandler);
 
-	/* shift the inside registers to the outside */
-	digitalWrite(PIN_OUTPUT,HIGH);
-	usleep(500);
-	digitalWrite(PIN_OUTPUT,LOW);
+    /* spin around and do nothing */
+    while(keepRunning) {
+        sleep(1);
+    }
 
-	/* write changes to logfile */
-	time_t rawtime;
-	struct tm * timeinfo;
-	time (&rawtime);
-	timeinfo = localtime(&rawtime);
-
-	file_ptr = fopen(LOG_FILE,"a");
-	if (!file_ptr) {
-		printf("Unable to open log file!\n");
-		return EXIT_FAILURE;
-	}
-
-	char logbuffer[100];
-	strftime(logbuffer,100,"%d.%m.%Y %H:%M:%S",timeinfo);
-	fprintf(file_ptr,"%s | %s%s | %s%s\n",logbuffer,bit_rep[before>>4], bit_rep[before & 0x0F],bit_rep[buffer[0]>>4], bit_rep[buffer[0] & 0x0F]);
-
-	fclose(file_ptr);
-
-	return EXIT_SUCCESS;
+    /* cleanup */
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    gpioTerminate();
+    return EXIT_SUCCESS;
 }
