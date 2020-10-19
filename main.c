43044ac8fddd1d9967876971c7e9be87d7f13501
Fabian Pflug <fabian.pflug@gummu.de>
2020-10-19 14:18:40 +0200

    Moved observe_pins to more config like pattern

--- main.c
+++ main.c
@@ -19,6 +19,9 @@
 #define PIN_OUTPUT 5
 #define STATE_FILE "/home/pi/light_state"
 
+static const int num_pins = 9;
+static const int[num_pins] observe_pins = [ 6,  18,  22,  13,  23,  24,  27,  17,  25 ];
+
 int initPinforObserve(int pin);
 void alertFunction(int gpio, int level, uint32_t tick);
 
@@ -245,15 +248,9 @@ int main(int argc, char* argv[]) {
     MQTTClient_subscribe(client, topicbuffer, QOS);
 
     /* set pins for observation */
-    initPinforObserve(6);
-    initPinforObserve(18);
-    initPinforObserve(22);
-    initPinforObserve(13);
-    initPinforObserve(23);
-    initPinforObserve(24);
-    initPinforObserve(27);
-    initPinforObserve(17);
-    initPinforObserve(25);
+    for(int i=0; i < num_pins; i++) {
+        initPinforObserve(observe_pins[i]);
+    }
 
     /* init signal handling */
     signal(SIGINT, intHandler);
